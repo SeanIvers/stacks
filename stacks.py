bd7ca2c851053d8c792ad696bdcd17741aac14be
SeanIvers <seanivers55@gmail.com>
2021-12-09 18:02:42 -0500

    Add push and pop methods

--- stacks.py
+++ stacks.py
@@ -11,4 +11,20 @@ class Stack:
         self.size = 0
         self.limit = limit
 
+    def push(self, value):
+        if self.has_space():
+            item = Node(value)
+            item.set_next_node(self.top_item)
+            self.top_item = item
+            self.size += 1
+        print("All out of space!")
+
+    def pop(self):
+        if not self.is_empty():
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        print("This stack is totally empty.")
+
     
